dc95533a3c4650538011dd2b2ae58659204bdb1b
Terra-01 <106461620+Terra-01@users.noreply.github.com>
2022-08-27 20:39:03 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const words = document.querySelector(".slider");
+const duration = 400;
+
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const animate = async () => {
+  await sleep(3000);
+  words.style.transform = `translateY(-25%)`;
+  words.style.transition = `transform ${duration}ms ease-in-out`;
+  await sleep(duration);
+  words.style.transform = `translateY(0%)`;
+  words.style.transition = `none`;
+  words.appendChild(words.firstElementChild);
+
+  animate();
+};
+animate();
