cd3d614969e51a32953ef7505aca4ccdd770aae4
Terra-01 <106461620+Terra-01@users.noreply.github.com>
2022-08-27 21:07:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed');
+  const words = document.querySelector(".slider");
+  const duration = 400;
+
+  const sleep = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+  const animate = async () => {
+    await sleep(2000);
+    words.style.transform = `translateY(-25%)`;
+    words.style.transition = `transform ${duration}ms ease-in-out`;
+    await sleep(duration);
+    words.style.transform = `translateY(0%)`;
+    words.style.transition = `none`;
+    words.appendChild(words.firstElementChild);
+
+    animate();
+  };
+  animate();
+});
